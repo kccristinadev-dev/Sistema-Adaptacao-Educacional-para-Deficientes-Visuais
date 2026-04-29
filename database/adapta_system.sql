8575de7bfda6d3cd8b8037eab2d3cd89d11b1664
Cristina <kc.cristinadev@gmail.com>
2026-04-29 18:25:47 -0300

    criação do banco de dados

--- database/adapta_system.sql
+++ database/adapta_system.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE adapta_system;
+USE adapta_system;
 
+CREATE TABLE professores (
+   id_professor INT PRIMARY KEY AUTO_INCREMENT,
+   nome VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   senha VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
