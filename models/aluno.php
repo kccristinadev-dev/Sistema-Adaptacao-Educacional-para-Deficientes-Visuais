10917aba03b2dead8b498dc97f3f2a2797396a64
Cristina <kc.cristinadev@gmail.com>
2026-05-12 19:07:18 -0300

    Atualizar o aluno.php

--- models/aluno.php
+++ models/aluno.php
@@ -4,7 +4,14 @@ private $conn;
 public function __construct($conexao){
 $this->conn = $conexao;
 }
+public function listarAluno(){
 
+$sql = "SELECT * FROM alunos";
+
+$stmt = $this->conn->query($sql);
+
+$stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 
 }
