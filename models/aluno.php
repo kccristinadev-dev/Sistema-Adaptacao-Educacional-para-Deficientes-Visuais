be118e44694a98e25eea30f96737c9014f46ec3e
Cristina <kc.cristinadev@gmail.com>
2026-05-12 19:23:55 -0300

    Atualizar o aluno.php

--- models/aluno.php
+++ models/aluno.php
@@ -14,7 +14,11 @@ return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 public function buscarAluno() {
 
-$sql = "SELECT alunos FROM id_aluno";
+$sql = "SELECT * FROM id_aluno where id_aluno : id_aluno";
+
+$stmt = $this->conn->query($sql);
+
+return $stmt->fetchAll(PDO:FETCH_ASSOC);
 
 
 }
