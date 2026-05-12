f2d5b3286ee69093dbb3bc96a8fb8fd69eaec4a3
Cristina <kc.cristinadev@gmail.com>
2026-05-12 19:54:52 -0300

    Atualizar o professor.php

--- models/professor.php
+++ models/professor.php
@@ -6,6 +6,33 @@ $this->conn = $conexao;
 }
 
 
+// Pega todos os alunos do banco
+public function listarProf(){
+
+$sql = "SELECT * FROM professores";
+
+$stmt = $this->conn->query($sql);
+
+
+return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+// Pega somente um aluno específico 
+public function buscarProf($id_professor) {
+
+$sql = "SELECT * FROM alunos where id_professor = :id_professor";
+
+$stmt = $this->conn->prepare($sql);
+$stmt->bindParam(':id_professor, $id_professor, PDO::PARAM_INT);
+$stmt->execute();
+
+return $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+}
+
+}
 
 }
 
