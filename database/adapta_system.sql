53819d96985e9a6212ddf9aed191d8c9b48078f8
Cristina <kc.cristinadev@gmail.com>
2026-07-22 17:44:39 -0300

    Atualizar o adapta_system.sql

--- database/adapta_system.sql
+++ database/adapta_system.sql
@@ -10,7 +10,7 @@ CREATE TABLE pessoas (
 cpf VARCHAR(25) NOT NULL UNIQUE, 
  telefone VARCHAR(20) NOT NULL,
    senha VARCHAR(255) NOT NULL,
-tipo ENUM('admin', 'professor', 'aluno'),
+tipo ENUM('admin','professor','aluno') NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
