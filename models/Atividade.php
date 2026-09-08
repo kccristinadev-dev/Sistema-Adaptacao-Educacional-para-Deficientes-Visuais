7648e68edab159d390b1e494bbe85ef864cb553c
Cristina <kc.cristinadev@gmail.com>
2026-09-08 07:43:49 -0300

    Update and rename Atividade to Atividade.php

---
+++ models/Atividade.php
@@ -0,0 +1,20 @@
+
+<?php
+class Atividade {
+private $conn;
+public function __construct($conexao){
+$this->conn = $conexao;
+}
+
+public function cadastrarAtividade(){
+
+
+}
+public function listarAtivivade(){
+
+}
+  public function DeletarAtividade(){
+    
+  }
+  
+}
