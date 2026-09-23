ffbdd4c09650838ddc5d33e471bf14c2fe95e2f6
Cristina <kc.cristinadev@gmail.com>
2026-09-23 16:18:02 -0300

    Refactor dashboard.php structure and add scripts

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require "../controllers/usuarioLogado.php";
 require "../config/conexao.php";
 ?>
@@ -19,7 +19,8 @@ require "../config/conexao.php";
  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=local_library,mic,school">
  
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/index.css">
+<link rel="stylesheet" href="../css/TelaProfessor.css">
+    <link rel="stylesheet" href="../css/corPadrao.css">
 
     <!-- Título -->
     <title>DORINA_Tela</title>
@@ -29,11 +30,27 @@ require "../config/conexao.php";
     
 </head>
 
-<body class="<?= $necessidade ?>">
-        <!-- CONTEÚDO PRINCIPAL -->
-    <main>
-      
-<h1>Olá, <?= htmlspecialchars($nome) ?>!</h1>  
+<body class="<?= htmlspecialchars($adptacao ?? '') ?>">        <!-- CONTEÚDO PRINCIPAL -->
+<main id="app">
+    <h1>Olá, <?= htmlspecialchars($nome) ?>!</h1>  
+
+</main> 
+<script>
+    const nome = <?= json_encode($nome) ?>;
+    const atividades = <?= json_encode($atividades ?? []) ?>;
+    const respostas = <?= json_encode($respostas ?? []) ?>;
+    const turmas = <?= json_encode($turmas ?? []) ?>;
+</script>
+
+<script src="<?= $jsTela ?>" defer></script>
+
+<?php foreach ($jsFuncoes as $arquivo): ?>
+    <script src="<?= htmlspecialchars($arquivo) ?>" defer>
+        
+    </script>
+<?php endforeach; ?>
+</body>
+</html>
         
         
         
